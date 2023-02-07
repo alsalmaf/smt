6dff903338dd3c83da64029077984fe01c370042
alsalmaf <56139553+alsalmaf@users.noreply.github.com>
2023-02-07 03:06:38 -0800

    Update setup.sh

--- lite/examples/object_detection/raspberry_pi/setup.sh
+++ lite/examples/object_detection/raspberry_pi/setup.sh
@@ -11,17 +11,17 @@ python3 -m pip install pip --upgrade
 python3 -m pip install -r requirements.txt
 
 # Download TF Lite models
-FILE=${DATA_DIR}/efficientdet_lite0.tflite
+FILE=${DATA_DIR}/efficientdet_lite4.tflite
 if [ ! -f "$FILE" ]; then
   curl \
-    -L 'https://storage.googleapis.com/download.tensorflow.org/models/tflite/task_library/object_detection/rpi/lite-model_efficientdet_lite0_detection_metadata_1.tflite' \
+    -L 'https://storage.googleapis.com/download.tensorflow.org/models/tflite/task_library/object_detection/rpi/lite-model_efficientdet_lite4_detection_metadata_1.tflite' \
     -o ${FILE}
 fi
 
-FILE=${DATA_DIR}/efficientdet_lite0_edgetpu.tflite
+FILE=${DATA_DIR}/efficientdet_lite4_edgetpu.tflite
 if [ ! -f "$FILE" ]; then
   curl \
-    -L 'https://storage.googleapis.com/download.tensorflow.org/models/tflite/task_library/object_detection/rpi/efficientdet_lite0_edgetpu_metadata.tflite' \
+    -L 'https://storage.googleapis.com/download.tensorflow.org/models/tflite/task_library/object_detection/rpi/efficientdet_lite4_edgetpu_metadata.tflite' \
     -o ${FILE}
 fi
 
